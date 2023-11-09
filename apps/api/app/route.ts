c610756bede01cdf2091c3aca7b20bc290475c3a
samyabrata-maji <samyabratamaji334@gmail.com>
2023-11-10 02:15:58 +0530

    feat: rm api app

--- apps/api/app/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(): NextResponse {
-  return NextResponse.json({ message: "hello world" }, { status: 200 });
-}
--- apps/extension/src/app.tsx
+++ apps/extension/src/app.tsx
@@ -1,7 +1,14 @@
-import { SignedIn, SignedOut, SignIn, SignUp, ClerkProvider } from '@clerk/chrome-extension';
-import { useNavigate, Routes, Route, MemoryRouter } from 'react-router-dom';
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignUp,
+  ClerkProvider,
+} from "@clerk/chrome-extension";
+import { useNavigate, Routes, Route, MemoryRouter } from "react-router-dom";
 
-let REACT_APP_CLERK_PUBLISHABLE_KEY="pk_test_bmV4dC1nYXJmaXNoLTQ2LmNsZXJrLmFjY291bnRzLmRldiQ"
+let REACT_APP_CLERK_PUBLISHABLE_KEY =
+  "pk_test_bmV4dC1nYXJmaXNoLTQ2LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 function HelloUser() {
   return <p> Hello user</p>;
@@ -11,35 +18,29 @@ function ClerkProviderWithRoutes() {
   const navigate = useNavigate();
 
   return (
-    <div className='w-[600px] h-[600px]'>
-    <ClerkProvider
-      publishableKey={REACT_APP_CLERK_PUBLISHABLE_KEY}
-      navigate={to => navigate(to)}
-      syncSessionWithTab
-    >
-      <Routes>
-        <Route
-          path='/sign-up/*'
-          element={<SignUp signInUrl='/' />}
-        />
-        <Route
-          path='/'
-          element={
-            <>
-              <SignedIn>
-                <HelloUser />
-              </SignedIn>
-              <SignedOut>
-                <SignIn
-                  afterSignInUrl='/'
-                  signUpUrl='/sign-up'
-                />
-              </SignedOut>
-            </>
-          }
-        />
-      </Routes>
-    </ClerkProvider>
+    <div className="w-[600px] h-[600px]">
+      <ClerkProvider
+        publishableKey={REACT_APP_CLERK_PUBLISHABLE_KEY}
+        navigate={(to) => navigate(to)}
+        syncSessionWithTab
+      >
+        <Routes>
+          <Route path="/sign-up/*" element={<SignUp signInUrl="/" />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <SignedIn>
+                  <HelloUser />
+                </SignedIn>
+                <SignedOut>
+                  <SignIn afterSignInUrl="/" signUpUrl="/sign-up" />
+                </SignedOut>
+              </>
+            }
+          />
+        </Routes>
+      </ClerkProvider>
     </div>
   );
 }
